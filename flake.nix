4936312aba220c6d0916db27d080ddc69d375b2e
catitw <catitw@outlook.com>
2025-12-10 10:48:04 +0800

    feat: add `01-create-keypair`

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "libssh dev shell";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+
+        packages = with pkgs; [
+          clang
+        ];
+
+        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+      };
+    };
+}
